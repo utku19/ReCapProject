f9eb5c7c9ce97a57a1d20991e079ea660a4c513a
utku berk gök <utku_19_berk@hotmail.com>
2021-04-02 02:07:28 +0300

    11. Gün Ödev

--- DataAccess/Concrete/EntityFramework/EfRentalDal.cs
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -1,6 +1,7 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,5 +10,9 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfRentalDal : EfEntityRepositoryBase<Rental, ReCapContext>, IRentalDal
     {
+        public List<RentalDetailDto> GetRentalDetails()
+        {
+            
+        }
     }
 }
